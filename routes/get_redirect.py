20f40c2fbc43c8f5f709160b0513209875613096
marcushaddon <haddon.marcus@gmail.com>
2018-02-18 12:45:13 -0600

    Dont record click data for links created by anons.

--- routes/get_redirect.py
+++ routes/get_redirect.py
@@ -14,31 +14,33 @@ def shorturls(short_code):
     if shorturl is None:
         abort(404)
     
-    # Record data about this click
-    linkid = shorturl['_id']
-    headers = request.headers
-    now = time.time()
-    # TODO: Get geolocation!
+    # Don't record info for links created by anons
+    if shorturl['user_id'] is not None:
+        # Record data about this click
+        linkid = shorturl['_id']
+        headers = request.headers
+        now = time.time()
 
-    headerdict = {}
-    for header in request.headers:
-        headerdict[header[0]] = header[1]
-    
-    gi = pygeoip.GeoIP('GeoLiteCity.dat', pygeoip.MEMORY_CACHE)
-    geo_data = gi.record_by_addr(request.remote_addr)
 
-    click = {
-        "url_id": linkid,
-        "headers": headerdict,
-        "time": now,
-        "geo_data": geo_data
-    }
-    
-    try:
-        db.clicks.insert_one(click)
-    except Exception, e:
-        # Most important thing is to serve the redirect
-        print e.message
-        pass
+        headerdict = {}
+        for header in request.headers:
+            headerdict[header[0]] = header[1]
+        
+        gi = pygeoip.GeoIP('GeoLiteCity.dat', pygeoip.MEMORY_CACHE)
+        geo_data = gi.record_by_addr(request.remote_addr)
+
+        click = {
+            "url_id": linkid,
+            "headers": headerdict,
+            "time": now,
+            "geo_data": geo_data
+        }
+        
+        try:
+            db.clicks.insert_one(click)
+        except Exception, e:
+            # Most important thing is to serve the redirect
+            print e.message
+            pass
     
     return redirect(shorturl["long_url"])
