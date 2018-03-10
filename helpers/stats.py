45341e8ce06b52f75d0071981884b64108daf317
marcushaddon <haddon.marcus@gmail.com>
2018-03-10 17:02:45 -0600

    PrConvert to dd the correct way.

--- helpers/stats.py
+++ helpers/stats.py
@@ -27,7 +27,7 @@ def update_stats(user_id, click, stat_level):
             headerstats = dd(lambda: dd(int), query)
         else:
             new = False
-            headerstats['devicetype'] = dd(int)
+            headerstats['devicetype'] = dd(int, headerstats['devicetype'])
 
         print "headerstats"
         print headerstats
