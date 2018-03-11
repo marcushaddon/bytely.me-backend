289297456b9ed53ad5b5860e9fe4864d55f97f38
marcushaddon <haddon.marcus@gmail.com>
2018-03-11 15:24:30 -0500

    Send JSON with error when signing up.

--- helpers/errors.py
+++ helpers/errors.py
@@ -1,7 +1,6 @@
 from flask import jsonify
 
 def error_response(code, message):
-    print "DOING ME"
     response = jsonify({'message': message})
     response.status_code = code
     return response
--- routes/signup.py
+++ routes/signup.py
@@ -2,6 +2,7 @@ import hashlib
 from flask import Blueprint, request, abort, jsonify
 from db import db
 from config import PASSWORD_SALT, JWT_SECRET
+from helpers.errors import error_response
 
 sign_up = Blueprint('sign_up', __name__)
 
@@ -17,7 +18,7 @@ def signup():
         password = body['password']
         email = body['email']
     except:
-        abort(400)
+        return error_response(400, 'Please fill out all required fields.')
     
     # TODO: Make sure email address is valid
     # Make sure username isnt taken
@@ -27,7 +28,7 @@ def signup():
         print e
 
     if existingusername is not None:
-        abort(409, "That username is already taken.")
+        return error_response(409, 'That username is already taken ;(')
     
     try:
         existingemail = db.users.find_one({ 'email': email })
@@ -35,7 +36,7 @@ def signup():
         print e
 
     if existingemail is not None:
-        abort(409, "An account is aleady registered for this email address.")
+        return error_response(409, "An account is aleady registered for this email address. Contact the Dev to ask him to reset your password or build a 'Forgot password feature'.")
     
     # TODO: create user as pending and send confirmation email
     pwhash = hashlib.sha512(password + PASSWORD_SALT).hexdigest()
@@ -52,6 +53,6 @@ def signup():
     except Exception, e:
         print "there was an error"
         print e
-        abort(500)
+        return error_response(500, 'Uh oh, something went wrong on our end... @_@')
     
     return jsonify({"result": "success"})
